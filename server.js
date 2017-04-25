39b4f0c0a91f08bbc9236769513497a572a4ad0b
Enxtur Enxbat <enxtur@Enxturs-iMac.local>
2017-04-25 20:18:16 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by enxtur on 4/19/17.
+ */
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+var count = 0
+app.get('/', function (req, res) {
+  console.log('count:', count)
+  res.send('count: ' + count++);
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
